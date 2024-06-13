f8768bf6d8bf3b2e88f035cd42c5417f47dd4b55
Rustam Nuriddinov <rustamm.nuriddinov@gmai.com>
2024-06-13 22:23:09 +0500

    start db connectioin

--- controllers/contact-controller.js
+++ controllers/contact-controller.js
@@ -1,15 +1,19 @@
+// O'rnatishdan maqsad try-catch ga olmaslik asinxron kodlarni
+// ushbu modul o'zi avtamatik try catch bilan ishlaydi
+const asyncHandler = require('express-async-handler');
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
-const getContacts = async (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
   // JSON qiymat qaytarish
   res.status(200).json({ message: 'Get all contacts broo ))' });
-};
+});
 
 //@desc Creata contact
 //@route POST /api/contacts
 //@access public
-const createContact = async (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
     console.log('bosh');
@@ -18,34 +22,34 @@ const createContact = async (req, res) => {
   }
   console.log(req.body);
   res.status(201).json({ message: 'Create contact' });
-};
+});
 
 //@desc Get contact
 //@route GET /api/contacts/:id
 //@access public
-const getContact = async (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json({ message: `Get contac for ${req.params.id}` });
-};
+});
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
 //@access public
-const updateContact = async (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json({ message: `Update contac for ${req.params.id}` });
-};
+});
 
 //@desc Delete contact
 //@route DEL /api/contacts/:id
 //@access public
-const deleteContact = async (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json({ message: `Delete contac for ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContacts,
